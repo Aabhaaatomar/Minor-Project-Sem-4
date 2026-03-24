198bbb83d56d6a85decd0e623ec39068fd911334
NishantRajawat562 <rajawatnishant562@gmail.com>
2026-03-24 22:44:27 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,21 @@
-# Routes package
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_socketio import SocketIO
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db = SQLAlchemy()
+jwt = JWTManager()
+migrate = Migrate()
+mail = Mail()
+socketio = SocketIO(cors_allowed_origins="*")
+
+
+def create_app():
+    app = Flask(__name__)
